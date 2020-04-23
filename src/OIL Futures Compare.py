459ee1614b0e77ef9c6b6bb4b42b250979daf95f
lucasgday <lucasgday@gmail.com>
2020-04-23 00:29:16 -0300

    OIL Futures Compare.py v2

--- src/OIL Futures Compare.py
+++ src/OIL Futures Compare.py
@@ -35,21 +35,22 @@ for i in range(1,13):
     table = pd.concat([table, data[-1:]],axis=0)
     tablePast = pd.concat([tablePast, data[:1]],axis=0)
 
-fig, ax = plt.subplots(figsize=(12,8), nrows=2, ncols=1, 
-                       gridspec_kw={'height_ratios':[2, 1]})
+fig, ax = plt.subplots(figsize=(12,8), nrows=3, ncols=1, 
+                       gridspec_kw={'height_ratios':[3, 1, 1]})
 ax[0].plot(table.index, table['Adj Close'], color='tab:blue', lw=3, 
            label='Today ({})'.format(today.date()))
 ax[0].plot(table.index, tablePast['Adj Close'], color='red', lw=2, ls='--', 
            label=str(daysAgo)+' Days Ago')
 ax[0].legend(loc='lower right', fontsize=14)
 ax[0].set_ylabel("USD / bbl")
-ax[0].set_title('WTI Futures')
 ax[1].bar(table.index, table['Volume'], width=0.5, color='tab:blue', alpha=0.75)
-ax[1].bar(table.index, table['Volume']+tablePast['Volume'], bottom=table['Volume'], 
-          width=0.5, color='red', alpha=0.75)
+ax[2].bar(table.index, tablePast['Volume'], width=0.5, color='red', alpha=0.25)
 ax[1].set_yscale('log')
-ax[1].set_ylabel("Traded Contracts")
+ax[2].set_yscale('log')
+ax[1].set_ylabel("Today's Volume")
+ax[2].set_ylabel(str(daysAgo)+" Days Ago")
 
 fig.subplots_adjust(hspace=0)
 plt.xticks(rotation=45, horizontalalignment='right', fontsize=12, color='lightgray')
+fig.suptitle("WTI Future Contracts", y=0.7, color="gold", fontsize=50, alpha=0.1)
 plt.show()
